41f07c4dc10ac91e20da23c0978a89feffdfe951
unknown <Rando.Tarkmeel@tktk.ee git config --global user.name>
2024-10-24 23:43:26 +0300

    add Functions code

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.WriteLine("Enter the operation (+/-):");
+char userOperator = char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a:");
+int firstNum = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter b:");
+int SecondNum = Int32.Parse(Console.ReadLine()); 
+
+switch (userOperator)
+{
+    case '+':
+        Addiotion(firstNum, SecondNum);
+        break;
+    case '-':
+        Subtraction(firstNum, SecondNum);
+        break;
+        default:
+        Console.WriteLine("invalid operator");
+        break;
+}
+
+
+
+static void Addiotion(int a, int b)
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+
+static void Subtraction(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Are you coming or leaving? (in or out):");
+string userchoice = Console.ReadLine();
+
+if(userchoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, alligator.");
+}
